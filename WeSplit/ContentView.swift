973ab3a92a1f05446f6a9431e0a1ae77863b5b76
NishantaNepal <nepalnishanta29@gmail.com>
2023-02-28 20:42:15 +0545

    Completed Bill splitting

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,19 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    @State private var checkAmount = 0.0;
+    @State private var numberOfPeople = 2;
+    @State private var tipPercentage = 20;
+    @FocusState private var amountIsFocused : Bool;
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    var totalPerPerson: Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage);
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    var body: some View {
+        NavigationView{
+            Form {
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code:Locale.current.identifier))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                        
+               
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<70){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+           
+                Section{
+                    Picker("Tip percentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id:\.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+            header:{
+                Text("How much tip do you like to leave?")
+            }
+           
+                Section {
+                    Text(totalPerPerson, format: .currency(code: "NPR"))
+                }
+            header:{
+                Text("Total bill per person")
+            }
+                
+                //
+                .navigationTitle("Nishant's Bill Sharing")
+                
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        //Spacer()
+                        Button("Done") {
+                            amountIsFocused = false
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
